ac217c97e60986167be29f593a48c0666db26253
BaekNothing <baeknothing@gmail.com>
2023-02-03 18:16:53 +0900

    edit : 문단 테스트 추가
    body : 문단 테스트 추가 및 쓰레드 테스트 항목 삭제

--- JongSungAutoSelecterTests/JSAutoSelecterTester.cs
+++ JongSungAutoSelecterTests/JSAutoSelecterTester.cs
@@ -45,12 +45,11 @@ public class JSAutoSelecterTester
     [Fact]
     public void SetParagraph()
     {
+        JSAutoSelecter selecter = new JSAutoSelecter();
+        string originStr = "테스트은(는) 상품은(는) 테스트을(를) 상품을(를) 테스트이(가) 상품이(가)";
+        Assert.Equal("테스트는 상품은 테스트를 상품을 테스트가 상품이", selecter.SetParagraph(originStr));
         
-    }
-
-    [Fact]
-    public void SetParagraphWithThread()
-    {
-           
+        originStr = "테스트은/는 상품은/는 테스트을/를 상품을/를 테스트이/가 상품이/가";
+        Assert.Equal("테스트는 상품은 테스트를 상품을 테스트가 상품이", selecter.SetParagraph(originStr));
     }
 }
